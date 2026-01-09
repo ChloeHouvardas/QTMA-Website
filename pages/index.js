28419108bef11564f3d7c8238695b839d91b9990
Aqothy <aq0335@gmail.com>
2026-01-08 22:02:41 -0500

    fix: add icon for google search results

--- pages/index.js
+++ pages/index.js
@@ -24,7 +24,9 @@ export default function Main() {
 					name="description"
 					content="QTMA hires 12 Developers, 4 Product Manager, 4 Designers, 8 Business Analysts to complete our product teams. We also hire various operating roles. Applications include a written application and interview with the incoming Co - Chairs."
 				/>
-				<link rel="icon" type="image/png" href="/favicon.ico" />
+				<link rel="icon" href="/favicon.ico" sizes="any" />
+				<link rel="icon" type="image/png" href="/assets/Club%20Data/QTMA_logo.png" />
+				<link rel="apple-touch-icon" href="/assets/Club%20Data/QTMA_logo.png" />
 				<title>QTMA</title>
 				<meta name="og:title" content={"QTMA"} />
 			</Head>
