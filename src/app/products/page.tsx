1ede7302d9ba9de89b61cf91af3f1f37e2d6e209
Jason Wu <jwu0592@gmail.com>
2026-09-12 21:56:25 -0400

    feat: add all pages and routes.

---
+++ src/app/products/page.tsx
@@ -0,0 +1,10 @@
+import { Header } from "@/components/Header";
+
+export default function ProductsPage() {
+	return (
+		<div id="#top">
+			<Header />
+			<h1>PRODUCTS</h1>
+		</div>
+	);
+}
---
+++ src/app/team/page.tsx
@@ -0,0 +1,10 @@
+import { Header } from "@/components/Header";
+
+export default function TeamPage() {
+	return (
+		<div id="#top">
+			<Header />
+			<h1>TEAM</h1>
+		</div>
+	);
+}
