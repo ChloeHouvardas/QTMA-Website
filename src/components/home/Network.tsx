fa1f6bbfbfa08da55c1cfaaeb675951585caa7fb
Jason Wu <jwu0592@gmail.com>
2026-09-17 21:05:38 -0400

    style: updated logos to be svg instead of png

--- src/components/home/Network.tsx
+++ src/components/home/Network.tsx
@@ -1,35 +1,85 @@
 import Image from "next/image";
 
-const placementLogos = [
-	{ alt: "Boston Consulting Group", src: "/assets/Placement Logos/bcg.png" },
+type PlacementLogo = {
+	alt: string;
+	className?: string;
+	containerClassName?: string;
+	src: string;
+};
+
+const placementLogos: PlacementLogo[] = [
+	{
+		alt: "Boston Consulting Group",
+		className: "h-10 w-auto max-w-full object-contain sm:h-8 lg:h-14",
+		src: "/assets/Visuals/Boston_Consulting_Group_2020_logo.svg",
+	},
 	{ alt: "NEXT Canada", src: "/assets/Placement Logos/next36.png" },
-	{ alt: "Amazon", src: "/assets/Placement Logos/amazon.png" },
-	{ alt: "Google", src: "/assets/Placement Logos/google.png" },
-	{ alt: "Uber", src: "/assets/Placement Logos/uber.png" },
-	{ alt: "Moelis", src: "/assets/Placement Logos/moelis.png" },
-	{ alt: "Shopify", src: "/assets/Placement Logos/shopify.png" },
-	{ alt: "Meta", src: "/assets/Placement Logos/meta.png" },
-	{ alt: "Tesla", src: "/assets/Placement Logos/tesla.png" },
-	{ alt: "Evercore", src: "/assets/Placement Logos/evercore.png" },
-	{ alt: "Morgan Stanley", src: "/assets/Placement Logos/morganstanley.png" },
-	{ alt: "McKinsey & Company", src: "/assets/Placement Logos/mckinsey.png" },
-	{ alt: "Microsoft", src: "/assets/Placement Logos/microsoft.png" },
-	{ alt: "Dropbox", src: "/assets/Placement Logos/dropbox.png" },
-	{ alt: "J.P. Morgan", src: "/assets/Placement Logos/jpmorgan.png" },
-	{ alt: "YouTube", src: "/assets/Placement Logos/youtube.png" },
-	{ alt: "Barclays", src: "/assets/Placement Logos/barclays.png" },
+	{ alt: "Amazon", src: "/assets/placement-logos-svg/amazon.svg" },
+	{ alt: "Google", src: "/assets/placement-logos-svg/google.svg" },
+	{
+		alt: "Uber",
+		className: "h-10 w-auto max-w-full object-contain sm:h-8 lg:h-14",
+		src: "/assets/placement-logos-svg/uber.svg",
+	},
+	{
+		alt: "Moelis",
+		className: "h-10 w-auto max-w-full object-contain sm:h-8 lg:h-14",
+		src: "/assets/placement-logos-svg/moelis.svg",
+	},
+	{
+		alt: "Shopify",
+		className: "h-16 w-auto max-w-full object-contain sm:h-16 lg:h-24",
+		src: "/assets/placement-logos-svg/shopify.svg",
+	},
+	{
+		alt: "Meta",
+		className: "h-10 w-auto max-w-full object-contain sm:h-8 lg:h-14",
+		src: "/assets/placement-logos-svg/meta.svg",
+	},
+	{ alt: "Tesla", src: "/assets/placement-logos-svg/tesla.svg" },
+	{ alt: "Evercore", src: "/assets/placement-logos-svg/evercore.svg" },
+	{
+		alt: "Morgan Stanley",
+		className: "h-16 w-auto max-w-full object-contain sm:h-20 lg:h-28",
+		containerClassName: "h-16 sm:h-20 lg:h-28",
+		src: "/assets/placement-logos-svg/morganstanely.svg",
+	},
+	{
+		alt: "McKinsey & Company",
+		src: "/assets/placement-logos-svg/mckinsey.svg",
+	},
+	{
+		alt: "Microsoft",
+		className: "h-16 w-auto max-w-full object-contain sm:h-16 lg:h-24",
+		src: "/assets/placement-logos-svg/microsoft.svg",
+	},
+	{ alt: "Dropbox", src: "/assets/placement-logos-svg/dropbox.svg" },
+	{ alt: "J.P. Morgan", src: "/assets/placement-logos-svg/jpmorgan.svg" },
+	{ alt: "YouTube", src: "/assets/placement-logos-svg/youtube.svg" },
+	{ alt: "Barclays", src: "/assets/placement-logos-svg/barclays.svg" },
 	{
 		alt: "Ontario Teachers'",
-		src: "/assets/Placement Logos/ontarioteachers.png",
+		className: "h-10 w-auto max-w-full object-contain sm:h-8 lg:h-14",
+		src: "/assets/placement-logos-svg/ontarioteachers.svg",
 	},
-	{ alt: "Wealthsimple", src: "/assets/Placement Logos/wealthsimple.png" },
-	{ alt: "PayPal", src: "/assets/Placement Logos/paypal.png" },
-	{ alt: "Cohere", src: "/assets/Placement Logos/cohere.png" },
-	{ alt: "Palantir", src: "/assets/Placement Logos/palantir.png" },
+	{
+		alt: "Wealthsimple",
+		src: "/assets/placement-logos-svg/wealthsimple.svg",
+	},
+	{ alt: "PayPal", src: "/assets/placement-logos-svg/paypal.svg" },
+	{ alt: "Cohere", src: "/assets/placement-logos-svg/cohere.svg" },
+	{ alt: "Palantir", src: "/assets/placement-logos-svg/palantir.svg" },
 	{ alt: "Aritzia", src: "/assets/Placement Logos/aritzia.png" },
-	{ alt: "Dior", src: "/assets/Placement Logos/dior.png" },
-	{ alt: "Financial Times", src: "/assets/Placement Logos/financialtimes.png" },
-	{ alt: "Harvard University", src: "/assets/Placement Logos/harvard.png" },
+	{
+		alt: "Dior",
+		className: "h-10 w-auto max-w-full object-contain sm:h-8 lg:h-14",
+		src: "/assets/placement-logos-svg/dior.svg",
+	},
+	{
+		alt: "Financial Times",
+		src: "/assets/placement-logos-svg/financialtimes.svg",
+	},
+	{ alt: "Harvard University", src: "/assets/placement-logos-svg/harvard.svg" },
 	{ alt: "Stanford University", src: "/assets/Placement Logos/stanford.png" },
 	{ alt: "Wharton School", src: "/assets/Placement Logos/wharton.png" },
 ];
@@ -63,12 +113,17 @@ export function Network() {
 				<div className="mx-auto grid w-full max-w-[1200px] grid-cols-2 items-center gap-x-6 gap-y-8 px-8 sm:grid-cols-3 sm:gap-x-8 sm:gap-y-10 sm:px-12 lg:grid-cols-4 lg:gap-x-10 lg:gap-y-12 lg:px-16">
 					{placementLogos.map((logo) => (
 						<div
-							className="flex h-14 items-center justify-center sm:h-16 lg:h-24"
+							className={`flex items-center justify-center ${
+								logo.containerClassName ?? "h-14 sm:h-16 lg:h-24"
+							}`}
 							key={logo.src}
 						>
 							<Image
 								alt={logo.alt}
-								className="h-14 w-auto max-w-full object-contain sm:h-12 lg:h-20"
+								className={
+									logo.className ??
+									"h-14 w-auto max-w-full object-contain sm:h-12 lg:h-20"
+								}
 								height={183}
 								sizes="(min-width: 1024px) 220px, (min-width: 640px) 25vw, 40vw"
 								src={logo.src}
